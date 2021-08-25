0b95af415166660dc504e9fbb134ec6cc54cc066
weaselflink <stefan@stefanbissell.de>
2021-08-25 15:46:11 +0200

    add ZonedDateTime example

---
+++ src/main/kotlin/talks/timepersistence/UsingZonedDateTime.kt
@@ -0,0 +1,32 @@
+package talks.timepersistence
+
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    val formatter = DateTimeFormatter
+        .ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    withDb {
+        val myTime = ZonedDateTime
+            .parse("2021-01-01T12:00:03+11:00[Australia/Sydney]")
+            .also { println(it) }
+        val asString = formatter.format(myTime)
+            .also { println(it) }
+        println()
+
+        update("CREATE TABLE testy (my_time timestamp, my_zone varchar)")
+        update("INSERT INTO testy VALUES ('$asString', '${myTime.zone}')")
+        query("SELECT my_time, my_zone FROM testy") { rs ->
+            while (rs.next()) {
+                LocalDateTime
+                    .parse(rs.getString(1), formatter)
+                    .atZone(ZoneId.of(rs.getString(2)))
+                    .also { println(it) }
+            }
+        }
+        update("DROP TABLE testy")
+    }
+}
