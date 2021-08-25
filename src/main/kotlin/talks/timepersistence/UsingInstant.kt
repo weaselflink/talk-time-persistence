0e5fe5dbf41c8d96976d2bea34b330653632e574
weaselflink <stefan@stefanbissell.de>
2021-08-25 15:34:50 +0200

    extend Instant example

--- src/main/kotlin/talks/timepersistence/UsingInstant.kt
+++ src/main/kotlin/talks/timepersistence/UsingInstant.kt
@@ -14,16 +14,19 @@ fun main() {
         val myTime = Instant.parse("2021-01-01T12:00:03Z")
             .also { println(it) }
         val asString = formatter.format(myTime)
+            .also { println(it) }
+        println()
 
         update("CREATE TABLE testy (my_time timestamp)")
+        update("INSERT INTO testy VALUES ('$myTime')")
         update("INSERT INTO testy VALUES ('$asString')")
-        query("SELECT my_time FROM testy") {
-            it.next()
-            println(
+        query("SELECT my_time FROM testy") { rs ->
+            while (rs.next()) {
                 ZonedDateTime
-                    .parse(it.getString(1), formatter)
+                    .parse(rs.getString(1), formatter)
                     .toInstant()
-            )
+                    .also { println(it) }
+            }
         }
         update("DROP TABLE testy")
     }
